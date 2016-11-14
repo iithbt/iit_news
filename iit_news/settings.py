c1e596e73e9291ca3f2c5da73503c059f53b4830
Chirag <csoni111@gmail.com>
2016-11-14 16:12:12 +0530

    adds news app
    - new NewsArticles model
    - postgres database settings

--- iit_news/settings.py
+++ iit_news/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'iit_news.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'chirag',
+        'PASSWORD': 'test1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
---
+++ news/models.py
@@ -0,0 +1,17 @@
+from django.utils.encoding import python_2_unicode_compatible
+from django.db import models
+
+# Create your models here.
+
+@python_2_unicode_compatible
+class NewsArticles(models.Model):
+	post_id = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    summary = models.CharField(max_length=500)
+    img_src = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    source = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
