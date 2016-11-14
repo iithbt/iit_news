cb4828125ac0c940a14244ee60303d8cadd2ff67
Chirag <csoni111@gmail.com>
2016-11-15 01:48:53 +0530

    adds redis_url to env

--- iit_news/settings.py
+++ iit_news/settings.py
@@ -143,8 +143,8 @@ STATICFILES_DIRS = (
 )
 
 # CELERY STUFF
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+BROKER_URL = os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
