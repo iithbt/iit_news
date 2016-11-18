a936b1f84bfe099458a44c905ec8d20d4657bba4
Chirag <csoni111@gmail.com>
2016-11-18 16:32:12 +0530

    adds CORS suport

--- iit_news/settings.py
+++ iit_news/settings.py
@@ -29,12 +29,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['iithbt.herokuapp.com','localhost']
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
 INSTALLED_APPS = [
     'news',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
