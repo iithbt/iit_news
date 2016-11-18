857d6036bf3f434cdb14deb94136ec1db47e17a1
Chirag <csoni111@gmail.com>
2016-11-18 20:14:37 +0530

    extends the db columns max length

--- news/models.py
+++ news/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 @python_2_unicode_compatible
 class NewsArticle(models.Model):
-	post_id = models.CharField(max_length=200)
-	title = models.CharField(max_length=200)
+	post_id = models.CharField(max_length=300)
+	title = models.CharField(max_length=300)
 	pub_date = models.DateTimeField('date published')
-	summary = models.CharField(max_length=500)
-	img_src = models.CharField(max_length=200,blank=True,null=True)
-	url = models.CharField(max_length=200)
+	summary = models.CharField(max_length=600)
+	img_src = models.CharField(max_length=300,blank=True,null=True)
+	url = models.CharField(max_length=300)
 	source = models.CharField(max_length=50)
 
 	def __str__(self):
