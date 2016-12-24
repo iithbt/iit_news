219754615c38c604d2c02c60cc2384f7784ec9b8
Chirag <csoni111@gmail.com>
2016-12-25 05:05:11 +0530

    adds code to send notification after fetch

--- news/grab_feed.py
+++ news/grab_feed.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.utils import timezone
 from .models import NewsArticle
 from feedparser import parse
+import requests
 
 import sys
 
@@ -39,8 +40,25 @@ def processFeedItem(item):
 		url = url,
 		source = source)
 	n.save()
+	sendNotification(title, summaryText, url, img_src)
 	return True
 
+def sendNotification(title, body, url, icon_url):
+	fcm_url = "https://fcm.googleapis.com/fcm/send"
+	headers = {
+		'Authorization': 'key=AIzaSyDNCsH4twaJc5JT5dZrq77JYkB0MhoIE2E',
+		'Content-Type':'application/json',
+		}
+	payload = { 
+		"notification": {
+    		"title": title,
+    		"body": body,
+    		"icon" : icon_url,
+    		"click_action" : url,
+  		},
+  		"to" : "/topics/iitnews"
+	}
+	r = requests.post(fcm_url, headers=headers, data=payload)
 
 def getPubDate(time_struct):
 	return datetime.fromtimestamp(mktime(time_struct),timezone.utc)
